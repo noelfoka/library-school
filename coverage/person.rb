76c95feeb7c525d7ccf10e2a526bd45f71c62448
noelfoka <nomgnenoel@gmail.com>
2023-10-03 02:43:54 +0100

    implement student

--- coverage/person.rb
+++ coverage/person.rb
@@ -1,19 +1,19 @@
 class Person
   attr_reader :id, :name, :age
 
-  def initialize(id, age, name = "Unknown", parent_permission: true)
-    @id = id
+  def initialize(age, name: "Unknown", parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
 
-  def age=(age)
-    @age = age
+  def age=(new_age)
+    @age = new_age
   end
 
   def can_use_services?
