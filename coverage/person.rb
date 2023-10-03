7ac9ab992659140c7a4f31b8a0acb54ffeb08e69
noelfoka <nomgnenoel@gmail.com>
2023-10-03 02:30:08 +0100

    update

--- coverage/person.rb
+++ coverage/person.rb
@@ -1,7 +1,7 @@
 class Person
   attr_reader :id, :name, :age
 
-  def initialize(id, age, name = "Unknown", parent_permission = true)
+  def initialize(id, age, name = "Unknown", parent_permission: true)
     @id = id
     @name = name
     @age = age
