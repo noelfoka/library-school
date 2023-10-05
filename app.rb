0b7abacf31bc976bc2e7c6f755770f73d97c86ed
noelfoka <nomgnenoel@gmail.com>
2023-10-06 00:01:48 +0100

    list all people in App class

--- app.rb
+++ app.rb
@@ -12,4 +12,12 @@ class App
         puts "#{book.title} by #{book.author}"
     end
   end
+
+  def list_people
+    @people.each do |person|
+      puts "#{person.name} #{person.type}"
+    end
+  end
+
+  
 end
