46d1dbdb5f66bae7adfd60a5ba979d7afb454b5b
noelfoka <nomgnenoel@gmail.com>
2023-10-05 01:42:08 +0100

    implement classroom class

--- student.rb
+++ student.rb
@@ -1,6 +1,9 @@
 require_relative 'person'
 
 class Student < Person
+
+  belongs_to :classroom
+  
   def initialize(age, classroom, name: 'Unknown', parent_permission: true)
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
