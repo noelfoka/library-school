152405b999b4f1a36425584fa778d8138e00f180
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-09 17:49:08 +0430

    Add showOptions class

--- options.rb
+++ options.rb
@@ -1,1 +1,13 @@
 # Show option implementation
+class ShowOptions
+  def show_options
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+end
