824216f8e7d422e0fa1d06e635e66b9165439ad1
noelfoka <nomgnenoel@gmail.com>
2023-10-11 19:58:42 +0100

    Finish to implement the Teacher class

--- teacher.rb
+++ teacher.rb
@@ -3,7 +3,7 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
