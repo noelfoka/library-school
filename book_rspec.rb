a6eed79954b730c98f4c4b994bb5c7f6127e09ba
noelfoka <nomgnenoel@gmail.com>
2023-10-11 17:36:51 +0100

    fix the error

--- book_rspec.rb
+++ book_rspec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'date'
 
 require_relative 'book'
 require_relative 'rental'
@@ -6,7 +7,8 @@ require_relative 'person'
 
 describe Book do
   let(:book) { Book.new('Title', 'Author') }
-  let(:person) { Person.new('Okari', 25) }
+  let(:person) { Person.new('Mahdi', 25) }
+  let(:date) {Date.new(2023, 10, 11)}
 
   context 'Initialization' do
     it 'should initialize with a title' do
@@ -36,15 +38,15 @@ describe Book do
 
   context 'add_rental method' do
     it 'should add a rental to the rentals array' do
-      rental = book.add_rental('2023-10-09', person)
+      rental = book.add_rental(person, date)
       expect(book.rentals).to include(rental)
     end
 
     it 'should create a new Rental object' do
-      rental = book.add_rental('2023-10-09', person)
+      rental = book.add_rental(person, date)
       expect(rental).to be_an_instance_of(Rental)
-      expect(rental.book).to eq(book)
-      expect(rental.person).to eq(person)
+      expect(rental.book).to eq(Book)
+      expect(rental.person).to eq(Person)
     end
   end
 end
