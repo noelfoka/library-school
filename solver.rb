ca16c2efb93ac3034973e5147e322e2e0a476d64
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-12 20:38:33 +0430

    Add reverse method

--- solver.rb
+++ solver.rb
@@ -4,4 +4,14 @@ class Solver
         return 1 if n == 0
         (1..n).reduce(:*)
     end
+
+    def reverse(word)
+        reversed_word = ''
+        i = word.length - 1
+        while i >= 0
+        reversed_word += word[i]
+        i -= 1
+        end
+        reversed_word
+    end
 end
