a1508aca013346d8399a98ef8ef8d9a6776422e8
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-12 20:56:40 +0430

    fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,31 +1,26 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError,'N must be a non-negatve integer' if n < 0
-        return 1 if n == 0
-        (1..n).reduce(:*)
-    end
+  def factorial(num)
+    raise ArgumentError, 'N must be a non-negatve integer' if num.negatve?
+    return 1 if num.zero?
 
-    def reverse(word)
-        reversed_word = ''
-        i = word.length - 1
-        while i >= 0
-        reversed_word += word[i]
-        i -= 1
-        end
-        reversed_word
+    (1..num).reduce(:*)
+  end
+
+  def reverse(word)
+    reversed_word = ''
+    i = word.length - 1
+    while i >= 0
+      reversed_word += word[i]
+      i -= 1
     end
-    
-    def fizzbuzz(n)
+    reversed_word
+  end
+
+  def fizzbuzz(num)
     result = ''
-    if n % 3 == 0
-      result += 'fizz'
-    end
-    if n % 5 == 0
-      result += 'buzz'
-    end
-    if result == ''
-      result = n.to_s
-    end
+    result += 'fizz' if num % 3.zero?
+    result += 'buzz' if num % 5.zero?
+    result = n.to_s if result == ''
     result
   end
 end
