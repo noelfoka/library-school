c861ab60a0be42e0f9366bc46354da7f4c546eee
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-12 18:47:40 +0430

    Add test for reverse method

--- rspec_test/solver_rspec.rb
+++ rspec_test/solver_rspec.rb
@@ -3,20 +3,35 @@ require_relative '../solver'
 
 describe Solver do 
     describe '#factorial' do 
-    it 'Returns the factorial of 5' do
-        expect(factorial(5)).to eq(120)
+        it 'Returns the factorial of 5' do
+            expect(factorial(5)).to eq(120)
+        end
+        it 'Returns the factorial of 0' do 
+            expect(factorial(0)).to eq(1)
+        end
+        it 'returns the factorial of 1' do 
+            expect(factorial(1)).to eq(1)
+        end
+        it 'returns the factorial of 10' do
+            expect(factorial(10)).to eq(3628800)
+        end
+        it 'riases an error when a negative number is passed' do 
+            expect {factorial(-1)}. to raise_error(ArgumentError, 'Argument must be a non-negative integer')
+        end
     end
-    it 'Returns the factorial of 0' do 
-        expect(factorial(0)).to eq(1)
+
+    describe '#reverse' do
+        it 'returns the reverse of "hello"' do
+            expect(reverse('hello')).to eq('olleh')
+        end
+        it 'returns the reverse of "world"' do 
+            expect(reverse('world')).to eq('dlrow')
+        end
+        it 'returns the reverse of an empty string' do 
+            expect(reverse('')).to eq('')
+        end
+        it 'returns the reverse of a string with one character' do
+            expect(reverse('a')).to eq('a')
+        end
     end
-    it 'returns the factorial of 1' do 
-        expect(factorial(1)).to eq(1)
-    end
-    it 'returns the factorial of 10' do
-        expect(factorial(10)).to eq(3628800)
-    end
-    it 'riases an error when a negative number is passed' do 
-        expect {factorial(-1)}. to raise_error(ArgumentError, 'Argument must be a non-negative integer')
-    end
-end
 end
