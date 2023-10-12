afe3caf2ca8337ee32bd2b3d017e71754c2641c6
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-12 20:37:57 +0430

    add factorial method in solver.rb file

--- rspec_test/solver_rspec.rb
+++ rspec_test/solver_rspec.rb
@@ -2,52 +2,51 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do 
+    let(:solver) {Solver.new}
     describe '#factorial' do 
         it 'Returns the factorial of 5' do
-            expect(factorial(5)).to eq(120)
+            expect(solver.factorial(5)).to eq(120)
         end
         it 'Returns the factorial of 0' do 
-            expect(factorial(0)).to eq(1)
+            expect(solver.factorial(0)).to eq(1)
         end
         it 'returns the factorial of 1' do 
-            expect(factorial(1)).to eq(1)
+            expect(solver.factorial(1)).to eq(1)
         end
         it 'returns the factorial of 10' do
-            expect(factorial(10)).to eq(3628800)
+            expect(solver.factorial(10)).to eq(3628800)
         end
         it 'riases an error when a negative number is passed' do 
-            expect {factorial(-1)}. to raise_error(ArgumentError, 'Argument must be a non-negative integer')
+            expect {solver.factorial(-1)}. to raise_error(ArgumentError, 'Argument must be a non-negative integer')
         end
     end
 
     describe '#reverse' do
         it 'returns the reverse of "hello"' do
-            expect(reverse('hello')).to eq('olleh')
+            expect(solver.reverse('hello')).to eq('olleh')
         end
         it 'returns the reverse of "world"' do 
-            expect(reverse('world')).to eq('dlrow')
+            expect(solver.reverse('world')).to eq('dlrow')
         end
         it 'returns the reverse of an empty string' do 
-            expect(reverse('')).to eq('')
+            expect(solver.reverse('')).to eq('')
         end
         it 'returns the reverse of a string with one character' do
-            expect(reverse('a')).to eq('a')
+            expect(solver.reverse('a')).to eq('a')
         end
     end
-
     describe '#fizzbuzz' do
         it 'returns "fizz" for numbers divisible by 3' do
-            expect(fizzbuzz(3)).to eq('fizz')
-            expect(fizzbuzz(6)).to eq('fizz')
+            expect(solver.fizzbuzz(3)).to eq('fizz')
+            expect(solver.fizzbuzz(6)).to eq('fizz')
         end
         it 'returns "buzz" for numbers divisible by 5' do
-            expect(fizzbuzz(5)).to eq('buzz')
-            expect(fizzbuzz(10)).to eq('buzz')
+            expect(solver.fizzbuzz(5)).to eq('buzz')
+            expect(solver.fizzbuzz(10)).to eq('buzz')
         end
         it 'returns "fizzbuzz" for numbers divisible by 3 and 5' do
-            expect(fizzbuzz(15)).to eq('fizzbuzz')
-            expect(fizzbuzz(30)).to eq('fizzbuss')
+            expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+            expect(solver.fizzbuzz(30)).to eq('fizzbuss')
         end
-    end
-    
+    end 
 end
--- solver.rb
+++ solver.rb
@@ -1,2 +1,7 @@
 class Solver
+    def factorial(n)
+        raise ArgumentError,'N must be a non-negatve integer' if n < 0
+        return 1 if n == 0
+        (1..n).reduce(:*)
+    end
 end
